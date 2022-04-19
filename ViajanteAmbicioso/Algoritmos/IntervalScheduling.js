c805e8670c3520a6b796168d820cdbe1135aad57
biancasofia <sofiabrasil44@gmail.com>
2022-04-19 15:10:13 -0300

    Create IntervalScheduling.js

---
+++ ViajanteAmbicioso/Algoritmos/IntervalScheduling.js
@@ -0,0 +1,16 @@
+module.exports = class IntervalScheduling {
+ 
+    calculaScheduling(qtd, horarios){
+        var max = [];
+        var anterior = 0
+
+        for (var i = 0; i < qtd; i++) {
+            if (horarios[i].inicio >= anterior) {
+                max.push(horarios[i])
+                anterior = horarios[i].fim
+            }
+        }
+        return max
+    }
+
+}
