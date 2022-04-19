4073abdebcff33d3296b416fc22743b1b29a7045
biancasofia <sofiabrasil44@gmail.com>
2022-04-19 15:22:58 -0300

    adicionando rotas

--- ViajanteAmbicioso/app.js
+++ ViajanteAmbicioso/app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
+const Graph = require('./graph/graph.js');
+const data = require('./data/routes.json');
 
-
-
+app.use(express.static('public'));
 
 
 
