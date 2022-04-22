7b477681bf0742e5302987f2048dd99fe34dbaa5
biancasofia <sofiabrasil44@gmail.com>
2022-04-22 12:53:27 -0300

    add nodes

--- ViajanteAmbicioso/app.js
+++ ViajanteAmbicioso/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const Graph = require('./Algoritmos/graph.js');
 const data = require('./Data/rotas.json');
+const rotaWH = require('./Data/rotasWH.json');
 const passeios = require('./Data/passeios.json');
 const intervalScheduling = require('./Algoritmos/IntervalScheduling.js');
 app.use(express.static('public'));
@@ -13,10 +14,26 @@ app.get('', (req, res) => {
 });
 
 
-console.log(data[0]["Aracaj�:Macei�"].distancia)
+const grafo = new Graph();
+for (let i = 0; i < rotaWH.length; i++) {
+    let objeto = rotaWH[i];
+    let name;
+    let objLiteral = {}
+    for (let chave in objeto) {
+        name = chave.substring(0, chave.indexOf(":"));
+        objLiteral[chave.substring(chave.indexOf(':') + 1)] = objeto[chave];
+    }
+    grafo.adicionaVertice(name, objLiteral);
+}
+
+let destinos = grafo.menorCaminho('Belo Horizonte', 'Natal').concat(['Belo Horizonte']).reverse()
+console.log(destinos)
 
 
-var cidadeAtual = 'Aracajú';
+//console.log(data[0]["Aracaj�:Macei�"].distancia)
+
+
+var cidadeAtual = destinos[destinos.length-1];
 var horarioChegada = '09:00';
 var horarios = [];
 for (var i = 0; i < passeios.length; i++) {
@@ -26,9 +43,9 @@ for (var i = 0; i < passeios.length; i++) {
         if (cidade == cidadeAtual) {
             for (var passeio in value) {
                 var value2 = value[passeio];
-                console.log(passeio)
+             //   console.log(passeio)
                 for (let j = 0; j < value2.length; j++) {
-                    console.log(value2[j].inicio + " - " + value2[j].fim);
+                //    console.log(value2[j].inicio + " - " + value2[j].fim);
                    var result = {
                         inicio: parseInt(value2[j].inicio.replace(':', '')),
                        fim: parseInt(value2[j].fim.replace(':', '')),
@@ -39,7 +56,7 @@ for (var i = 0; i < passeios.length; i++) {
                 }
 
             }
-            console.log(horarios)
+        //    console.log(horarios)
             horarios.sort(function (hor1, hor2) {
                 if (hor1.fim < hor2.fim) return -1;
                 if (hor1.fim > hor2.fim) return 1;
