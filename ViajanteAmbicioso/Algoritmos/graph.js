810dde0f155e1f84ffdce4ff0da241a78c019168
biancasofia <sofiabrasil44@gmail.com>
2022-04-22 17:35:09 -0300

    add logica de grade de passeios

--- ViajanteAmbicioso/Algoritmos/graph.js
+++ ViajanteAmbicioso/Algoritmos/graph.js
@@ -7,6 +7,7 @@ module.exports = class Graph {
 
     adicionaVertice(name, edges) {
         this.vertices[name] = edges;
+       
     };
 
     menorCaminho(inicio, fim) {
--- ViajanteAmbicioso/app.js
+++ ViajanteAmbicioso/app.js
@@ -8,12 +8,9 @@ const intervalScheduling = require('./Algoritmos/IntervalScheduling.js');
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.set('views', './public');
-
-app.get('', (req, res) => {
-    res.render('index', { showResult: false });
-});
-
-
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 const grafo = new Graph();
 for (let i = 0; i < rotaWH.length; i++) {
     let objeto = rotaWH[i];
@@ -26,50 +23,80 @@ for (let i = 0; i < rotaWH.length; i++) {
     grafo.adicionaVertice(name, objLiteral);
 }
 
-let destinos = grafo.menorCaminho('Belo Horizonte', 'Natal').concat(['Belo Horizonte']).reverse()
-console.log(destinos)
+app.get('', (req, res) => {
+    res.render('index', { showResult: false });
+});
+var origem, destino, passeioCadaCidade, horaPartida;
+app.post('/rota', (req, res) => {
+     origem = req.body.origem;
+     destino = req.body.destino;
+     passeioCadaCidade = req.body.queroPassear;
+     horaPartida = parseInt(req.body.partidaHora.replace(':', ''));
+    var interval = new intervalScheduling()
+   // console.log(horaPartida)
+    let destinos = grafo.menorCaminho(origem, destino).concat([origem]).reverse()
+    console.log(' Sua rota é ' + destinos)
+    var l = 0;
+ 
+    while (destinos.length > l) {
+ 
+        var ultimaCidade = destinos[destinos.length - 1];
+        var cidadeAtual = destinos[l];
+      //  if (passeioCadaCidade != 'on') {cidadeAtual=ultimaCidade}
+        var horarios = [];
+        console.log('cidade ' + l + ' '+ destinos[l]);
+        for (var i = 0; i < passeios.length; i++) {
+            var obj = passeios[i];
+            for (var cidade in obj) {
+                var value = obj[cidade];
+                if (cidade == cidadeAtual) {
+                    for (var passeio in value) {
+                        var value2 = value[passeio];
+                        //   console.log(passeio)
+                        for (let j = 0; j < value2.length; j++) {
+                            //    console.log(value2[j].inicio + " - " + value2[j].fim);
+                            var result = {
+                                inicio: parseInt(value2[j].inicio.replace(':', '')),
+                                fim: parseInt(value2[j].fim.replace(':', '')),
+                                passeio: passeio
+                            }
 
+                            horarios.push(result)
+                        }
 
-//console.log(data[0]["Aracaj�:Macei�"].distancia)
-
-
-var cidadeAtual = destinos[destinos.length-1];
-var horarioChegada = '09:00';
-var horarios = [];
-for (var i = 0; i < passeios.length; i++) {
-    var obj = passeios[i];
-    for (var cidade in obj) {
-        var value = obj[cidade];
-        if (cidade == cidadeAtual) {
-            for (var passeio in value) {
-                var value2 = value[passeio];
-             //   console.log(passeio)
-                for (let j = 0; j < value2.length; j++) {
-                //    console.log(value2[j].inicio + " - " + value2[j].fim);
-                   var result = {
-                        inicio: parseInt(value2[j].inicio.replace(':', '')),
-                       fim: parseInt(value2[j].fim.replace(':', '')),
-                       passeio: passeio
                     }
+                    //console.log(horarios)
+                    horarios.sort(function (hor1, hor2) {
+                        if (hor1.fim < hor2.fim) return -1;
+                        if (hor1.fim > hor2.fim) return 1;
+                        return 0;
+                    })
+
+                    var horariosScheduling = interval.calculaScheduling(horarios.length, horarios, horaPartida)
+                
                     
-                    horarios.push(result)
+                 /*   if (passeioCadaCidade != 'on') {
+                        console.log(horariosScheduling)
+                        break;
+                    } 
+                    else {
+                        console.log(horariosScheduling)
+                    }*/
+                    console.log(horariosScheduling)
+
                 }
 
             }
-        //    console.log(horarios)
-            horarios.sort(function (hor1, hor2) {
-                if (hor1.fim < hor2.fim) return -1;
-                if (hor1.fim > hor2.fim) return 1;
-                return 0;
-            })
-             var interval = new intervalScheduling()
-            var horariosScheduling = interval.calculaScheduling(horarios.length, horarios, parseInt(horarioChegada.replace(':', '')))
-            console.log(horariosScheduling)
-
         }
-
+        l++
     }
-}
+    res.render('index', { showResult: false });
+})
+
+
+
+
+
 
 
 
