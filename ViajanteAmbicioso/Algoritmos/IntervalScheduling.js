dfbb7eb1a97933f44116170afc297cd45f27178a
biancasofia <sofiabrasil44@gmail.com>
2022-04-23 13:42:48 -0300

    update interval

--- ViajanteAmbicioso/Algoritmos/IntervalScheduling.js
+++ ViajanteAmbicioso/Algoritmos/IntervalScheduling.js
@@ -3,11 +3,14 @@ module.exports = class IntervalScheduling {
     calculaScheduling(qtd, horarios, horarioAtual){
         var max = [];
         var anterior = 0
-
+        var passeioAnterior='';
         for (var i = 0; i < qtd; i++) {
-            if (horarios[i].inicio >= anterior && horarios[i].inicio>=horarioAtual) {
+            
+            if (horarios[i].inicio >= anterior && horarios[i].inicio >= horarioAtual && horarios[i].passeio != passeioAnterior) {
                 max.push(horarios[i])
                 anterior = horarios[i].fim
+                passeioAnterior = horarios[i].passeio;
+               
             }
         }
         return max
